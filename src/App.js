ce0b4444e024be58ff2bc0a512894ad8d9e350b9
antonmontrezor <montrezoranton991@gmail.com>
2021-03-13 12:03:00 -0500

    deleted unnecessary files, built some logic to make the first API call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+    const [line1Count, setLine1Count] = useState(5);
+    const [wordsLine1, setWordsLine1] = useState('');
+
+    const handleChange = event => {
+        setWordsLine1(event.target.value);
+        
+        fetch(`http://api.datamuse.com/sug?s=${wordsLine1}&max=10`)
+        .then(response => response.json())
+        .then(jsonResponse => console.log(jsonResponse))
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>hi friends</h1>
-      </header>
+      <h1>Haikus Highway</h1>
+      <form action="">
+          <label htmlFor="userWord"></label>
+          <input type="text" id="userWord" onChange={handleChange}/>
+      </form>
     </div>
   );
 }
